79c273c240e75f9294f38813b68ed8253222fabf
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-05-03 21:48:21 +0800

    add fist app

---
+++ repeat_task_with_time_delay/app/src/main/java/com/example/qiaolunzhang/firstapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.qiaolunzhang.firstapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mInterval = 5000; // 5 seconds by default can be  changed later
+    private Handler mHandler;
+    private TextView textView;
+    private int num = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.num_to_increase);
+
+        mHandler = new Handler();
+        startRepeatingTask();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                num = num + 1;
+                textView.setText(""+num+num);
+            } finally {
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
+}
